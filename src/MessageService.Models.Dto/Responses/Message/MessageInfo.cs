1de1e45491a9cbe3d29a78958ff053d8e631028e
vasis <vasily.condratiev@yandex.ru>
2023-03-03 06:41:11 +0300

    Added MessageService Added two endpoints (GetMessage, CreateMessage) Added migrations

---
+++ src/MessageService.Models.Dto/Responses/Message/MessageInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageService.Models.Dto.Responses.Message;
+
+public class MessageInfo
+{
+  public long Id { get; set; }
+
+  public string Content { get; set; }
+
+  public long ReceiverId { get; set; }
+
+  public long CreatedBy { get; set; }
+
+  public DateTime CreatedAtUtc { get; set; }
+
+  public DateTime? ModifiedAtUtc { get; set; }
+}
---
+++ src/MessageService/Controllers/MessageController.cs
@@ -0,0 +1,28 @@
+﻿using MessageService.Business.Commands.Message.Interfaces;
+using MessageService.Models.Db.Requests;
+using MessageService.Models.Dto.Responses;
+using MessageService.Models.Dto.Responses.Message;
+using Microsoft.AspNetCore.Mvc; 
+
+namespace MessageService.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class MessageController : ControllerBase
+{
+  [HttpPost("create")]
+  public async Task<OperationResultResponse<long?>> CreateAsync(
+    [FromServices] ICreateMessageCommand command,
+    [FromBody] CreateMessageRequest request)
+  {
+    return await command.ExecuteAsync(request);
+  }
+
+  [HttpGet("Get")]
+  public async Task<OperationResultResponse<MessageInfo?>> GetAsync(
+    [FromServices] IGetMessageCommand command,
+    [FromQuery] long messageId)
+  {
+    return await command.ExecuteAsync(messageId);
+  }
+}
